92c933487bc972065b16c4d04bce2f8a732642b3
Jiri Pulpan <jiripulpan10@google.com>
2019-05-10 15:53:56 +0200

    OOP in PHP example

--- index.php
+++ index.php
@@ -156,14 +156,195 @@ var_dump(get_class_vars('Test'));
 // use of extends
 class ParentClass {
     public $prop = 'I am Parent class';
+    private $privateProp = 'I am Parent private property';
+    static $staticProp = 'I am static from Parent';
+    function getPrivateProp() {
+        return $this->privateProp;
+    }
+    function __construct() {
+        self::$staticProp = 100;
+    }
+    final function finalFunc() {
+        echo 'Final function called';
+    }
+    public function overrideThis() {
+        echo 'Override this functions echo<br>';
+    }
 }
 class ChildClass extends ParentClass {
     public $propChild = 'I am Child class';
+    function __construct() {
+        self::$staticProp = 'I am static from Child';
+    }
+    function overrideThis() {
+        parent::overrideThis();
+        echo '&emsp;Overridden function from parent';
+    }
 }
-$childObject = new ChildClass();
-echo '<br><b>Property from parent: </b>';
-echo $childObject->prop;
+class GrandChildClass extends ChildClass {
+    function getProp() {
+        return self::$staticProp; // object is created from parent to child and then grandchild
+    }
+}
+$grandChildObject = new GrandChildClass();
+echo '<br><b>Parent property from grand child: </b>';
+echo $grandChildObject->prop;
+echo '<br><b>Static property from grand child: </b>';
+echo $grandChildObject->getProp();
 
+// child cant inherit from final class
+// parent::__construct(args) from child __construct(args)
+
+// private is as protected but cant be inherited
+echo '<br><b>Private property from inherited Parent class: </b>';
+echo $grandChildObject->getPrivateProp();
+
+// final function call, final function cant be overridden
+echo '<br><b>Call final function from parent: </b>';
+$grandChildObject->finalFunc();
+
+// overriding example
+echo '<br><b>Override example: </b>';
+$grandChildObject->overrideThis();
+
+// abstract class cant be instantiated with new keyword
+// abstract is to force programmer to implement classes with predefined function content
+abstract class ParentAbstractClass {
+    public $property;
+    function __construct() {
+        $this->property = 'Value set by constructor';
+    }
+    // declared only in abstract classes
+    abstract function abstractMethod();
+}
+class Child extends ParentAbstractClass {
+    function abstractMethod() {
+        echo 'This is abstract method set';
+    }
+}
+$childObject = new Child();
+echo '<br><b>Abstract class child: </b>';
+echo $childObject->property;
+
+// abstract function
+echo ' <br><b>Abstract method example: </b>';
+$childObject->abstractMethod();
+
+// interface is only to force programmer to implement specific classes
+interface Description {
+    // constants
+    const FIRSTNAME = 'John';
+    // non-defined methods
+    function weight();
+    function height();
+    function width();
+}
+interface OtherDescription {
+    const MIDDLENAME = 'Darius';
+}
+// with trait dont have to implement and we can override functions
+trait DescriptionTrait {
+    public $car = 'Tesla';
+    function getCar() {
+        return $this->car;
+    }
+}
+class TestClass implements Description, OtherDescription {
+    use DescriptionTrait; // use trait to use its content
+    const LASTNAME = 'Wicked';
+    function weight() {
+        echo 'Weight function';
+    }
+    function height() {
+        echo 'Weight function';
+    }
+    function width() {
+        echo 'Weight function';
+    }
+}
+$newObject = new TestClass();
+echo '<br><b>Implemented interface constant: </b>';
+echo TestClass::MIDDLENAME;
+echo '<br><b>Use keyword example: </b>';
+echo $newObject->getCar();
+
+echo '<br>';
+// mysql OOP login example
+class Login {
+    protected $server = 'localhost';
+    protected $user = 'root';
+    protected $pass = '';
+    protected $db = 'oop_example';
+    protected $conn;
+    public $errors = array();
+
+    function __construct() {
+        $this->conn = mysqli_connect($this->server, $this->user, $this->pass, $this->db);
+    }
+    protected function checkInput($var) {
+        $var = htmlspecialchars($var);
+        $var = trim($var);
+        $var = stripslashes($var);
+        return $var;
+    }
+    public function insertIntoTb($uname, $upass) {
+        $username = $this->checkInput($uname);
+        $userpass = $this->checkInput($upass);
+        // call for errors checking
+        if($this->checkErrors($username, $userpass)) {
+            // check for username in DB
+            if($this->checkUsername($username)) {
+                // method for insertion into DB
+                if($this->insertDB($username, $userpass)) $this->errors = ['Successfully inserted into DB'];
+            }
+        }
+    }
+    protected function checkErrors($uname, $upass) {
+        if(strlen($uname) > 10) {
+            array_push($this->errors, 'Username has more than 10 characters');
+            return false;
+        }
+        if(strlen($upass) < 4) {
+            array_push($this->errors, 'Password has less than 4 characters');
+            return false;
+        }
+        return true;
+    }
+    protected function insertDB($uname, $upass) {
+        $query = "INSERT INTO users(uname, upass) VALUES('".$uname."', '".$upass."')";
+        mysqli_query($this->conn, $query);
+        if(mysqli_affected_rows($this->conn) > 0) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    protected function checkUsername($username) {
+        $query = "SELECT uname from users where uname='".$username."'";
+        mysqli_query($this->conn, $query);
+        if(mysqli_affected_rows($this->conn) > 0) {
+            array_push($this->errors, "Username already in our DB");
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+if(isset($_POST['send'])) {
+    $login = new Login();
+    $login->insertIntoTb($_POST['username'], $_POST['password']);
+    foreach($login->errors as $error) {
+        echo $error;
+    }
+}
+?>
+<!-- html form -->
+<form method='post' action='#'>
+    <input type='text' name='username'>
+    <input type='password' name='password'>
+    <input type='submit' name='send'>
+</form>
+<?php
 // __destruct automatically
-echo '<br><b>$object gets destroyed on the end of the script automatically </b>';
+echo '<br><b>$object gets destroyed on the end of the script automatically: </b>';
 ?>
